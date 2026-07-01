af3ff40c168a7807347949390477ec42215ffea7
marthala nithin prasanna kumar reddy <147224496+Nithinreddy3093@users.noreply.github.com>
2026-07-01 13:05:12 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (54.69%) - LeetSync

---
+++ 2608-count-the-digits-that-divide-a-number/count-the-digits-that-divide-a-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countDigits(int num) {
+        int temp = num, count = 0;
+        while(temp != 0) {
+            int ld = temp % 10;
+            if(num % ld == 0) {
+                count++;
+            }
+            temp = temp / 10;
+        }
+        return count;
+    }
+}
