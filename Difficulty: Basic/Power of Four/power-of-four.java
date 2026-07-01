605e185ea38a8c3eea64579a07d3999b1de40c87
marthala nithin prasanna kumar reddy <147224496+Nithinreddy3093@users.noreply.github.com>
2026-07-01 14:27:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Power of Four/power-of-four.java
@@ -0,0 +1,12 @@
+class Solution {
+    boolean isPowerOfFour(int n) {
+        // code here
+        if(n <=0) {
+            return false;
+        }
+        while(n % 4 == 0) {
+            n /= 4;
+        }
+        return n == 1;
+    }
+}
