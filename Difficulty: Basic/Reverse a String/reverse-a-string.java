5c302029455eb9a1b90ef379c016a9873e817959
marthala nithin prasanna kumar reddy <147224496+Nithinreddy3093@users.noreply.github.com>
2026-07-06 17:25:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Reverse a String/reverse-a-string.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String reverseString(String s) {
+
+        StringBuilder ans = new StringBuilder();
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            ans.append(s.charAt(i));
+        }
+
+        return ans.toString();
+    }
+}
