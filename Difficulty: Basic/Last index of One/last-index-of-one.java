3b3519a18c0b4c80b03b8b02c1f38d95b2c5190a
marthala nithin prasanna kumar reddy <147224496+Nithinreddy3093@users.noreply.github.com>
2026-07-07 17:23:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Last index of One/last-index-of-one.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int lastIndex(String s) {
+        int index = -1;
+        
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '1') {
+                index = i;
+            }
+        }
+        return index;
+    }
+}
