da01bbb397ff83149dc4884540b470133bb998c7
marthala nithin prasanna kumar reddy <147224496+Nithinreddy3093@users.noreply.github.com>
2026-07-20 22:01:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Diamond Shape Pattern/diamond-shape-pattern.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        // Upper Half
+        for (int i = 1; i <= n; i++) {
+
+            // Spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // Stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+
+        // Lower Half
+        for (int i = n; i >= 1; i--) {
+
+            // Spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // Stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
